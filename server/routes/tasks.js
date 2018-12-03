bbaeedf2892ca0f20fddc9ace24cce12c40b16a6
vinutama <muhammadkhevin@gmail.com>
2018-12-03 19:32:23 +0700

    debug refresh on signup

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -2,17 +2,18 @@ var express = require('express');
 var router = express.Router();
 const taskController = require('../controllers/taskController')
 const checkLogin = require('../middleware/checkLogin')
+const checkOwner = require('../middleware/checkOwner')
 
 //Add New Task
-router.post('/add', checkLogin, taskController.addTask)
+router.post('/add', checkLogin, checkOwner, taskController.addTask)
 
 // //Find All Tasks
 router.get('/', checkLogin, taskController.findAll)
 
 // //Update status Task to done
-router.put('/:id', checkLogin, taskController.updateTask)
+router.put('/:id', checkLogin, checkOwner, taskController.updateTask)
 
 // //Delete task from user
-router.delete('/:id', checkLogin, taskController.deleteTask)
+router.delete('/:id', checkLogin, checkOwner, taskController.deleteTask)
 
 module.exports = router;
